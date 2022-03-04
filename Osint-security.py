ab60c73c745d49ea50be111d2123da169a4eed22
Crypt0n1c <80151193+crypt0n1@users.noreply.github.com>
2022-03-03 20:04:55 -0600

    Update Osint-security.py

--- Osint-security.py
+++ Osint-security.py
@@ -128,8 +128,8 @@ $$ |  $$ |$$\   $$ |  $$ |  $$ |\$$$ |   $$ |       $$\   $$ |$$ |      $$ |  $$
         print("Domain info: ")
         print("Your username is: {}  |   Your mail provider is: {} ".format(user_name, domain_name))
         correo = input("Para mas informacion del correo presione la Q")
-    if correo == "q":
-        webbrowser.open("https://whatismyipaddress.com/trace-email")
+    if correo == "Q":
+        webbrowser.open_new_tab("https://whatismyipaddress.com/trace-email")
     
         retu6 = input("Presione enter para salir")
 
